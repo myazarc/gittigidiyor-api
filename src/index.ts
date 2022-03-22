ae77eb4a0dbe724f39efdc827846f38e50fc823e
myazarc <myazarc@yandex.com>
2022-03-22 12:22:28 +0300

    renamed folder and export main

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import ApplicationService from "./gittigidiyor/Application/ApplicationService";
+import DeveloperService from "./gittigidiyor/Developer/DeveloperService";
+import CategoryService from "./gittigidiyor/Category/CategoryService";
+import CategoryV2Service from "./gittigidiyor/Category/CategoryV2Service";
+import ProductService from "./gittigidiyor/Product/ProductService";
+
+const n11 = { ApplicationService, DeveloperService, CategoryService, CategoryV2Service, ProductService };
+
+export default n11;
+export { ApplicationService, DeveloperService, CategoryService, CategoryV2Service, ProductService };
+module.exports = n11;
