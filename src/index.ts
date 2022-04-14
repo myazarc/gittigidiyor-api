ac7acd8e76c0f3ea367314b8b5e3932bb222742e
myazarc <myazarc@yandex.com>
2022-04-14 15:16:15 +0300

    fixed export

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import DeveloperService from "./gittigidiyor/Developer/DeveloperService";
 import CategoryService from "./gittigidiyor/Category/CategoryService";
 import CategoryV2Service from "./gittigidiyor/Category/CategoryV2Service";
 import ProductService from "./gittigidiyor/Product/ProductService";
-import OrderService from "./gittigidiyor/Product/OrderService";
+import OrderService from "./gittigidiyor/Order/OrderService";
 
-const gg = { ApplicationService, DeveloperService, CategoryService, CategoryV2Service, ProductService,OrderService };
+const gg = { ApplicationService, DeveloperService, CategoryService, CategoryV2Service, ProductService, OrderService };
 
 export default gg;
-export { ApplicationService, DeveloperService, CategoryService, CategoryV2Service, ProductService,OrderService };
+export { ApplicationService, DeveloperService, CategoryService, CategoryV2Service, ProductService, OrderService };
 module.exports = gg;
